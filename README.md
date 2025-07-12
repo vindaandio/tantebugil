d1af7291f52198a0bb71a8859363e619f0097812
vindaandio <163581395+vindaandio@users.noreply.github.com>
2025-07-12 09:22:57 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# tantebugil
-tantebugil
+
+[tantebugil](https://tantebugil.pages.dev)
---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const url = 'https://tantebugil.pages.dev';
+export const nama = 'Tante Bugil';
+export const desk = 'Video Bokep Indo Jepang Terbaru Simontok Bocil Ngentot Jilbab Smp Viral Sma Tante';
+export const terbit = '2024-06-10T00:01:03Z';
+
+export const google = 'kJd-P4WpHjy8CCsVEw7KVDTcwQT27UjQ7KmtpUNuUmc';
+export const yandex = 'd99b87c591456070';
+
+export const index = 'f4dc29c1-67f3-4732-a39a-d891493aeba8';
